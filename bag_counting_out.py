fa7f71b884ac89d5271e7b41ca9cee6f9b8a9201
LOKESH <156942244+Lokesh-chinnadurai@users.noreply.github.com>
2024-08-16 18:29:09 +0530

    Update bag_counting_out.py

--- bag_counting_out.py
+++ bag_counting_out.py
@@ -3,11 +3,9 @@ import numpy as np
 import os
 from ultralytics import YOLO
 
-# Load the YOLO model
-model = YOLO('loaded_bag.pt')  # Replace with your model path
+model = YOLO('t_bag.pt')#upload the pt file here
 
-# Open the video file
-video_path = 'D:\vChanel\truck\truck_bag_1.avi'  # Replace with your video path
+video_path = r'Bale_Count.mp4'#path for the video
 cap = cv2.VideoCapture(video_path)
 
 # Get video properties
@@ -16,12 +14,14 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # Define the codec and create VideoWriter object
-output_path = 'truck_bag_1.mp4'
+output_path = 'bale_output.mp4'#name for the video to save
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
 
 # Define the zone coordinates
-zone = [(1450, 425), (1450, 700), (1650, 700), (1650, 425)]
+# zone = [(78,203), (1805,205), (1805,250), (81,257)]
+zone = [(1040,21), (1139,19), (1141,1026), (1038,1028)]#adjust the coordinates based on the video
+
 
 # Initialize a counter for objects entering the zone
 object_count = 0
@@ -31,7 +31,7 @@ previous_count = 0
 recent_frames = []
 
 # Create an output directory if it doesn't exist
-output_folder = 'truck_bag_1'
+output_folder = 'bale_output'
 os.makedirs(output_folder, exist_ok=True)
 
 # Function to check if a frame is recent
@@ -55,7 +55,7 @@ while cap.isOpened():
     for result in results:
         for box in result.boxes:
             # Filter by confidence
-            if box.conf >= 0.50:
+            if box.conf >= 0.60:
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
                 class_id = int(box.cls)
 
@@ -64,7 +64,7 @@ while cap.isOpened():
                 height = y2 - y1
 
                 # Draw the bounding box
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 0), 2)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), -1)
 
                 # Calculate and draw the centroid
                 centroid_x = int((x1 + x2) / 2)
@@ -73,27 +73,29 @@ while cap.isOpened():
 
                 # Display the confidence score
                 confidence_text = f"{box.conf.item():.2f}"  # Convert tensor to float
-                cv2.putText(frame, confidence_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1)
+                # cv2.putText(frame, confidence_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1)
 
                 # Display the size of the bounding box
                 size_text = f"W: {width} H: {height}"
-                cv2.putText(frame, size_text, (x1, y1 - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+                # cv2.putText(frame, size_text, (x1, y1 - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
                 # Check if the size of the bounding box is greater than 200x200
-                if width > 200 and height > 200:
+                if width > 50 and height > 50:
                     # Check if the centroid is inside the zone
                     if cv2.pointPolygonTest(np.array(zone, np.int32), (centroid_x, centroid_y), False) >= 0:
                         if not is_frame_recent(frame_index, recent_frames):
                             object_count += 1
+
+
                             recent_frames.append(frame_index)  # Mark this frame as counted
                             # Mark the object as counted by drawing a different color
-                            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 2)
+                            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 0), 5)
 
     # Draw the zone bounding box
     cv2.polylines(frame, [np.array(zone, np.int32)], isClosed=True, color=(0, 255, 0), thickness=2)
 
     # Display the count
-    cv2.putText(frame, f"Count: {object_count}", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 255), 2)
+    cv2.putText(frame, f"Count: {object_count}", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 5)
 
     # Check if the object count has increased
     if object_count > previous_count:
